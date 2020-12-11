556998533372932717f6eb28bcfb0385ddbae298
shein <kur.ser@mail.ru>
2020-12-11 21:37:06 +0300

    5) Added application windows

--- src/sudoku/Login.java
+++ src/sudoku/Login.java
@@ -1,4 +1,46 @@
+
 package sudoku;
 
-public class Login {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+
+public class Login implements Initializable{
+
+    @FXML Button playBtn;
+    @FXML Button btnExit;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+    public void playBtnPressed() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("../fxml/layout.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 440, 600);
+        Stage stage = new Stage();
+        stage.setTitle("SUDOKU");
+        stage.setScene(scene);
+        stage.show();
+        Stage stage2 = (Stage) playBtn.getScene().getWindow();
+        stage2.close();
+    }
+    public void exitButtonPressed(){
+        Stage stage2 = (Stage) btnExit.getScene().getWindow();
+        stage2.close();
+    }
+    public int at(){
+        return 0;
+    }
+
 }
--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -1,23 +1,30 @@
 package sudoku;
 
+import java.io.IOException;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Sudoku extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../fxml/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../fxml/login.fxml"));
+        Scene scene = new Scene(root, 440, 600);
+        primaryStage.setTitle("Sudoku");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
+
